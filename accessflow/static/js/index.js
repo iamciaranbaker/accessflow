bdaf1321fe02060e5d07cc60abb2d3833c077c94
Ciaran Baker <ciaran.baker@capgemini.com>
2025-01-09 17:53:28 +0000

    feat: update frontend js and css handling

--- accessflow/static/js/index.js
+++
@@ -1,4 +0,0 @@
-import "./src/sidebar.js";
-import "./src/search.js";
-import "./src/password_reveal.js";
-import "./src/create_user.js";
---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+import "./src/css/main.css";
+
+import "./src/js/sidebar.js";
+import "./src/js/search.js";
+import "./src/js/password_reveal.js";
+import "./src/js/create_user.js";
