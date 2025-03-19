59537b7d7ca574c211984578d0afd1a6cead23d8
Ciaran Baker <ciaran.baker1@hmrc.gov.uk>
2025-03-19 18:09:20 +0000

    feat: add requeststatus enum

--- accessflow/models/request.py
+++ accessflow/models/request.py
@@ -1,4 +1,10 @@
+from enum import Enum
 from accessflow import db
+
+class RequestStatus(Enum):
+    PENDING = "pending"
+    APPROVED = "approved"
+    DECLINED = "declined"
 
 class Request(db.Model):
     # Table Name
@@ -7,6 +13,7 @@ class Request(db.Model):
     # Columns
     id = db.Column(db.Integer, autoincrement = True, primary_key = True, unique = True, nullable = False)
     name = db.Column(db.String(50), nullable = False)
+    status = db.Column(db.Enum(RequestStatus))
     created_at = db.Column(db.DateTime, default = db.func.now())
     updated_at = db.Column(db.DateTime, default = db.func.now(), onupdate = db.func.now())
 
