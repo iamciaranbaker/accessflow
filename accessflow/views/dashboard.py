f94f04add538af2c801a259d7493913e7973ca2b
Ciaran Baker <ciaran.baker1@hmrc.gov.uk>
2025-03-27 21:55:25 +0000

    feat: update dashboard to reflect actual statistics

--- accessflow/views/dashboard.py
+++ accessflow/views/dashboard.py
@@ -1,10 +1,20 @@
 from flask import render_template
 from flask.views import View
 from flask_login import login_required
+from accessflow.models.request import Request
+from accessflow.models.pid import PID
+from accessflow.models.team import Team
+from accessflow.models.service import Service
 
 class DashboardView(View):
     methods = ["GET"]
     decorators = [login_required]
 
     def dispatch_request(self):
-        return render_template("pages/dashboard.html")
+        counts = {
+            "requests": Request.query.count(),
+            "pids": PID.query.count(),
+            "teams": Team.query.count(),
+            "services": Service.query.count()
+        }
+        return render_template("pages/dashboard.html", counts = counts)
