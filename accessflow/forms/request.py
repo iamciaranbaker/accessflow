138134eaa81071e46494e951561677ccf0281cce
Ciaran Baker <ciaran.baker1@hmrc.gov.uk>
2025-04-03 14:21:52 +0100

    feat: update account creation request form

--- accessflow/forms/request.py
+++ accessflow/forms/request.py
@@ -4,68 +4,66 @@ from wtforms.validators import DataRequired, Length
 from accessflow.models.team import Team
 from accessflow.models.service import Service
 
-class AccountCreationRequestForm(FlaskForm):
-    name = StringField("Name", [DataRequired(message = "You must enter a name."), Length(min = 2, max = 50, message = "Your name is invalid.")])
-    team = SelectField("Team")
-    environments = SelectMultipleField("Environments", choices = [("nonprod", "Non-Production"), ("prod", "Production")])
-    nonprod_pid = StringField("Non-Production PID") # Accomodate CG and U. - e.g. cg19321, u.8904064
-    nonprod_ssh_key = TextAreaField("Non-Production SSH Key")
-    prod_pid = StringField("Production PID") # Accomodate U. and LSS. - e.g. u.8904064, LSS.8904064
-    prod_ssh_key = TextAreaField("Production SSH Key")
-    sc_clearance = SelectField("Do you have active SC clearance?", choices = [("true", "Yes"), ("false", "No")], validate_choice = False)
+class BaseRequestForm(FlaskForm):
+    name = StringField("Name", [
+        DataRequired(message = "You must enter a name."),
+        Length(min = 2, max = 50, message = "Your name is invalid.")
+    ])
+    environments = SelectMultipleField("Environments", choices = [
+        ("nonprod", "Non-Production"),
+        ("prod", "Production")
+    ])
+    nonprod_pid = StringField("Non-Production PID")
+    prod_pid = StringField("Production PID")
+    sc_clearance = SelectField(
+        "Do you have active SC clearance?",
+        choices = [("true", "Yes"), ("false", "No")],
+        validate_choice = False
+    )
     submit = SubmitField("Create Request")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Dynamically populate teams from database
-        self.team.choices = [(str(team.id), team.friendly_name) for team in Team.query.all()]
-
-class ServiceAccessRequestForm(FlaskForm):
-    name = StringField("Name", [DataRequired(message = "You must enter a name."), Length(min = 2, max = 50, message = "Your name is invalid.")])
-    services = SelectMultipleField("Services")
-    environments = SelectMultipleField("Environments", choices = [("nonprod", "Non-Production"), ("prod", "Production")])
-    nonprod_pid = StringField("Non-Production PID") # Accomodate CG and U. - e.g. cg19321, u.8904064
-    prod_pid = StringField("Production PID") # Accomodate U. and LSS. - e.g. u.8904064, LSS.8904064
-    sc_clearance = SelectField("Do you have active SC clearance?", choices = [("true", "Yes"), ("false", "No")], validate_choice = False)
-    justification = TextAreaField("Justification", [DataRequired()])
-    submit = SubmitField("Create Request")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Dynamically populate services from database
-        self.services.choices = [(str(service.id), service.name) for service in Service.query.all()]
-
-    # Override validate_on_submit method to add additional custom validation
     def validate_on_submit(self, extra_validators = None):
-        # Call the base validate_on_submit method first
         if not super().validate_on_submit(extra_validators = extra_validators):
             return False
-        
-        if not self.environments.data or len(self.environments.data) < 1:
-            # Manually add validation error for environments field
+
+        if not self.environments.data:
             self.environments.errors.append("You must select at least one environment.")
             return False
-        
+
         if "nonprod" in self.environments.data:
-            if not self.nonprod_pid.data or self.nonprod_pid.data.strip() == "":
-                # Manually add validation error for nonprod_pid field
+            if not self.nonprod_pid.data or not self.nonprod_pid.data.strip():
                 self.nonprod_pid.errors.append("You must enter a non-production PID.")
                 return False
             if not (7 <= len(self.nonprod_pid.data.strip()) <= 9):
                 self.nonprod_pid.errors.append("Your non-production PID is invalid.")
                 return False
-            
+
         if "prod" in self.environments.data:
-            if not self.prod_pid.data or self.prod_pid.data.strip() == "":
-                # Manually add validation error for prod_pid field
+            if not self.prod_pid.data or not self.prod_pid.data.strip():
                 self.prod_pid.errors.append("You must enter a production PID.")
                 return False
             if not (9 <= len(self.prod_pid.data.strip()) <= 11):
                 self.prod_pid.errors.append("Your production PID is invalid.")
                 return False
             if not self.sc_clearance.data or self.sc_clearance.data != "true":
-                # Manually add validation error for sc_clearance field
                 self.sc_clearance.errors.append("You must have valid SC Clearance to request access to production environments.")
                 return False
-            
+
         return True
+
+class AccountCreationRequestForm(BaseRequestForm):
+    team = SelectField("Team")
+    nonprod_ssh_key = TextAreaField("Non-Production SSH Key")
+    prod_ssh_key = TextAreaField("Production SSH Key")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.team.choices = [(str(team.id), team.friendly_name) for team in Team.query.all()]
+
+class ServiceAccessRequestForm(BaseRequestForm):
+    services = SelectMultipleField("Services")
+    justification = TextAreaField("Justification", [DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.services.choices = [(str(service.id), service.name) for service in Service.query.all()]
--- frontend/src/js/create_request.js
+++ frontend/src/js/create_request.js
@@ -1,63 +1,92 @@
 // Only run if we are on the frontend create request page
 if (window.location.pathname.toLowerCase().startsWith("/requests/create")) {
-    const environmentSelect = document.querySelector('select[name="environments"]');
+    const queryParams = new URLSearchParams(window.location.search);
+    const requestType = queryParams.get("type");
 
-    const nonprodPIDInput = document.querySelector('input[name="nonprod_pid"]');
-    const prodPIDInput = document.querySelector('input[name="prod_pid"]');
-    const scClearanceInputs = document.querySelectorAll('input[name="sc_clearance"]');
+    if (queryParams && requestType && (requestType == "account_creation" || requestType == "service_access")) {
+        const environmentSelect = document.querySelector('select[name="environments"]');
 
-    const nonprodPIDGroup = nonprodPIDInput.closest(".form-group");
-    const prodPIDGroup = prodPIDInput.closest(".form-group");
-    const scClearanceGroup = scClearanceInputs[0]?.closest(".form-group");
+        const nonProdPIDInput = document.querySelector('input[name="nonprod_pid"]');
+        const nonProdSSHKeyInput = document.querySelector('textarea[name="nonprod_ssh_key"]');
+        const prodPIDInput = document.querySelector('input[name="prod_pid"]');
+        const prodSSHKeyInput = document.querySelector('textarea[name="prod_ssh_key"]');
+        const scClearanceInputs = document.querySelectorAll('input[name="sc_clearance"]');
 
-    // Store original min/max values so we can restore them later
-    const nonprodMin = nonprodPIDInput.getAttribute("minlength");
-    const nonprodMax = nonprodPIDInput.getAttribute("maxlength");
-    const prodMin = prodPIDInput.getAttribute("minlength");
-    const prodMax = prodPIDInput.getAttribute("maxlength");
+        const nonProdPIDGroup = nonProdPIDInput.closest(".form-group");
+        const prodPIDGroup = prodPIDInput.closest(".form-group");
+        const scClearanceGroup = scClearanceInputs[0]?.closest(".form-group");
+        if (requestType == "account_creation" && nonProdSSHKeyInput) var nonProdSSHKeyGroup = nonProdSSHKeyInput.closest(".form-group");
+        if (requestType == "account_creation" && prodSSHKeyInput) var prodSSHKeyGroup = prodSSHKeyInput.closest(".form-group");
 
-    // Activate Select2
-    $(".select2").select2();
+        // Store original min/max values so we can restore them later
+        const nonProdMin = nonProdPIDInput.getAttribute("minlength");
+        const nonProdMax = nonProdPIDInput.getAttribute("maxlength");
+        const prodMin = prodPIDInput.getAttribute("minlength");
+        const prodMax = prodPIDInput.getAttribute("maxlength");
 
-    function updatePIDVisibility() {
-        const selectedValues = Array.from(environmentSelect.selectedOptions).map(opt => opt.value);
+        // Activate Select2
+        $(".select2").select2();
 
-        // Toggle Non-Prod PID
-        if (selectedValues.includes("nonprod")) {
-            nonprodPIDGroup.style.display = "block";
-            nonprodPIDInput.setAttribute("required", "");
-            nonprodPIDInput.setAttribute("minlength", nonprodMin);
-            nonprodPIDInput.setAttribute("maxlength", nonprodMax);
-        } else {
-            nonprodPIDGroup.style.display = "none";
-            nonprodPIDInput.removeAttribute("required");
-            nonprodPIDInput.removeAttribute("minlength");
-            nonprodPIDInput.removeAttribute("maxlength");
+        function updateEnvironmentVisibility() {
+            const selectedValues = Array.from(environmentSelect.selectedOptions).map(opt => opt.value);
+
+            // Toggle Non-Prod PID
+            if (selectedValues.includes("nonprod")) {
+                nonProdPIDGroup.style.display = "block";
+                nonProdPIDInput.setAttribute("required", "");
+                nonProdPIDInput.setAttribute("minlength", nonProdMin);
+                nonProdPIDInput.setAttribute("maxlength", nonProdMax);
+
+                if (requestType == "account_creation") {
+                    nonProdSSHKeyGroup.style.display = "block";
+                    nonProdPIDInput.setAttribute("required", "");
+                }
+            } else {
+                nonProdPIDGroup.style.display = "none";
+                nonProdPIDInput.removeAttribute("required");
+                nonProdPIDInput.removeAttribute("minlength");
+                nonProdPIDInput.removeAttribute("maxlength");
+
+                if (requestType == "account_creation") {
+                    nonProdSSHKeyGroup.style.display = "none";
+                    nonProdPIDInput.removeAttribute("required");
+                }
+            }
+
+            // Toggle Prod PID and SC Clearance
+            if (selectedValues.includes("prod")) {
+                prodPIDGroup.style.display = "block";
+                prodPIDInput.setAttribute("required", "");
+                prodPIDInput.setAttribute("minlength", prodMin);
+                prodPIDInput.setAttribute("maxlength", prodMax);
+
+                if (requestType == "account_creation") {
+                    prodSSHKeyGroup.style.display = "block";
+                    prodPIDInput.setAttribute("required", "");
+                }
+
+                scClearanceGroup.style.display = "block";
+                scClearanceInputs.forEach(input => input.setAttribute("required", ""));
+            } else {
+                prodPIDGroup.style.display = "none";
+                prodPIDInput.removeAttribute("required");
+                prodPIDInput.removeAttribute("minlength");
+                prodPIDInput.removeAttribute("maxlength");
+
+                if (requestType == "account_creation") {
+                    prodSSHKeyGroup.style.display = "none";
+                    prodPIDInput.removeAttribute("required");
+                }
+
+                scClearanceGroup.style.display = "none";
+                scClearanceInputs.forEach(input => input.removeAttribute("required"));
+            }
         }
 
-        // Toggle Prod PID and SC Clearance
-        if (selectedValues.includes("prod")) {
-            prodPIDGroup.style.display = "block";
-            prodPIDInput.setAttribute("required", "");
-            prodPIDInput.setAttribute("minlength", prodMin);
-            prodPIDInput.setAttribute("maxlength", prodMax);
+        // Initial state on page load
+        updateEnvironmentVisibility();
 
-            scClearanceGroup.style.display = "block";
-            scClearanceInputs.forEach(input => input.setAttribute("required", ""));
-        } else {
-            prodPIDGroup.style.display = "none";
-            prodPIDInput.removeAttribute("required");
-            prodPIDInput.removeAttribute("minlength");
-            prodPIDInput.removeAttribute("maxlength");
-
-            scClearanceGroup.style.display = "none";
-            scClearanceInputs.forEach(input => input.removeAttribute("required"));
-        }
+        // Attach change event to select element
+        $(environmentSelect).on("change", updateEnvironmentVisibility);
     }
-
-    // Initial state on page load
-    updatePIDVisibility();
-
-    // Attach change event to select element
-    $(environmentSelect).on("change", updatePIDVisibility);
 }
