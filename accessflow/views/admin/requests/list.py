8845a64456a301d2992146e19f98f6846f0afc55
Ciaran Baker <ciaran.baker1@hmrc.gov.uk>
2025-04-03 21:20:19 +0100

    feat: add search and pagination to requests

--- accessflow/views/admin/requests/list.py
+++ accessflow/views/admin/requests/list.py
@@ -1,23 +1,54 @@
-from flask import request, abort, render_template
+from flask import request, abort, redirect, url_for, render_template
 from flask.views import View
 from flask_login import login_required
 from accessflow.models.request import Request
-from accessflow.models.pid import PID, PIDEnvironmentType
-from accessflow.models.request_pid import RequestPID
 
 class AdminRequestListView(View):
     methods = ["GET"]
     decorators = [login_required]
 
     def dispatch_request(self):
-        req_id = request.args.get("id")
-        if not req_id:
-            return render_template("pages/admin/requests/list.html", requests = Request.query.all())
-        req = Request.query.filter(Request.id == req_id).first()
-        if not req:
-            abort(404)
-        #nonprod_pids = req.nonprod_pids.filter(RequestPID.pid.has(environment_type=PIDEnvironmentType.NONPROD)).order_by(RequestPID.confidence.desc()).all()
-        # for pid in nonprod_pids:
-        #     print(pid.pid)
-        print(req.nonprod_pids)
-        return render_template("pages/admin/requests/view.html", req = req)
+        request_id = request.args.get("id")
+        # Check if a request ID query parameter has been passed 
+        if request_id:
+            # Fetch the request based on the ID
+            requesto = Request.query.filter(Request.id == request_id).first()
+            # If no request was found with the given ID, return a 404 error
+            if not requesto:
+                abort(404)
+
+            return render_template("pages/admin/requests/view.html", requesto = requesto)
+
+        requests = Request.query
+
+        # Check if a search query parameter is present
+        search = request.args.get("q")
+        # Check if search query parameter is empty, i.e. empty search field submitted
+        if search is not None and not search.strip():
+            # Redirect to base requests page for cleaner URL
+            return redirect(url_for("admin/requests"))
+        if search:
+            # Allow the query to search for anything containing the search term
+            search = f"%{search}%"
+            # Filter against name
+            requests = requests.filter(
+                Request.name.like(search)
+            )
+
+        # Check if a filter has been applied for type
+        filtered_types = request.args.getlist("filter[type]")
+        if filtered_types:
+            requests = requests.filter(Request.type.in_(filtered_types))
+
+        # Check if a filter has been applied for status
+        filtered_statuses = request.args.getlist("filter[status]")
+        if filtered_statuses:
+            requests = requests.filter(Request.status.in_(filtered_statuses))
+
+        # Order requests by created date
+        requests = requests.order_by(Request.created_at.desc())
+
+        # Paginate the returned requests
+        requests = requests.paginate(per_page = None if request.args.get("per_page") else 8, max_per_page = 30)
+
+        return render_template("pages/admin/requests/list.html", requests = requests)
